1c1fadb63e3c34e8d443f9c5ddd4b3c9d61876a5
EdwinF01011 <efortega62@misena.edu.co>
2022-05-20 13:59:39 -0500

    ruotes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',function(){
+    return '<h1>ruta</h1>';
+});
+
+Route::get('/usuarios',function(){
+    return '<h1>Mostrando detalle del usuario</h1>'.$_GET['id'];
+});
+
+
+
+
+
+?>
