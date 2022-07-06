e75ad36a49e3aec088593752f11596660a2bb9e4
Edwin Fernando Ortega Diaz <efortega62@misena.edu.co>
2022-07-05 21:58:01 -0500

    remplazo de toda la plantilla

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-
-// die($_SERVER['REQUEST_URI']);//permite ver todo lo que hay después del dominio
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,52 +13,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/',function(){
-    return '<h1>ruta</h1>';
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
-
-// Route::get('/usuarios',function(){
-//     $text= "<h1>Mostrando detalle del usuario ".$_GET['id']." </h1>";// con php plano
-//     return $text;
-// });
-
-Route::get('/usuarios/{ide}/{name?}',[UserController::class, 'añadir']
-// function($ide,$name=null){
-// if($name){
-//     return 'usuarios';
-//     // return "<h1> Mostrando detalle del usuario {$ide} nombre: {$name} </h1> ";
-// }else{
-//     return "<h1> Mostrando detalle del usuario {$ide} </h1> ";
-// }
-
-// return "<h1> Mostrando detalle del usuario {$ide} nombre: {$name} </h1> ";
-// }
-)->where(['name' => '[\d]+' ,'ide'=>'[-\w]+'] );//'ide', '[0-9]+' , 
-
-
-Route::get('/usu',[UserController::class, '_usu']);
-
-// Route::get('/usuarios/{ide}/{name?}',function($ide,$name=null){
-//     if($name){
-//         return "<h1> Mostrando detalle del usuario {$ide} nombre: {$name} </h1> ";
-//     }else{
-//         return "<h1> Mostrando detalle del usuario {$ide} </h1> ";
-//     }
-//         // return "<h1> Mostrando detalle del usuario {$ide} nombre: {$name} </h1> ";
-//     })->where('ide', '[-\w]+');
-
-Route::get('/usuarios/{slug}',function($slug){
-    return "<h1> Esto es un slug {$slug} </h1> ";
-})->where(['slug' => 'cre   ate|delete|update']);
-
-
-Route::get('/usuarios',[UserController::class, 'index']);
-
-//[PagesController::UserController, 'index']
-?>
